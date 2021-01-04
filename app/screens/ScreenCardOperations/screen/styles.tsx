b680309e892e0e279a18b00e3171eea2855a3ba2
KAMRONBEK <kamuranbek1998@gmail.com>
2021-01-04 15:59:52 +0500

    page refactor

--- app/screens/ScreenCardOperations/screen/styles.tsx
+++ app/screens/ScreenCardOperations/screen/styles.tsx
@@ -4,25 +4,26 @@ import { Palette } from 'styles';
 const { width } = Dimensions.get('window');
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  scrollContainer: {
-    flexGrow: 1,
-    backgroundColor: Palette.white
-  },
-  greenBg: {
-    height: 350,
-    width,
-    position: 'absolute',
-    top: -100
-  },
-  greenInnerBg: {
-    height: 290,
-    width,
-    position: 'absolute',
-    top: -100,
-    borderBottomLeftRadius: 16,
-    borderBottomRightRadius: 16
-  }
+	container: {
+		flex: 1
+	},
+	scrollContainer: {
+		flexGrow: 1,
+		backgroundColor: Palette.white,
+		borderWidth: 1
+	},
+	greenBg: {
+		height: 350,
+		width,
+		position: 'absolute',
+		top: -100
+	},
+	greenInnerBg: {
+		height: 290,
+		width,
+		position: 'absolute',
+		top: -100,
+		borderBottomLeftRadius: 16,
+		borderBottomRightRadius: 16
+	}
 });
