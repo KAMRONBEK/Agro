73171b2e955c783e4b61144f1aefb5ff5b6b7f3d
OriHero <aka.orihero@gmail.com>
2021-01-16 15:31:55 +0500

    Logs

--- app/App.tsx
+++ app/App.tsx
@@ -6,16 +6,17 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import FlashMessage from 'react-native-flash-message';
-import { Provider } from 'react-redux';
-import { AppAuthWithNavigator, AppLoggedInWithNavigator } from 'router';
-import { store } from 'store';
-import reactotron from 'store/reactotron-config';
-import { apiQwerty, isTokenExist } from 'utils';
-import { AppLoadingView } from 'widgets/ModuleAppLoading';
-import { MyStatusBar } from 'widgets/ModuleShared';
+import AsyncStorage from "@react-native-community/async-storage";
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import FlashMessage from "react-native-flash-message";
+import { Provider } from "react-redux";
+import { AppAuthWithNavigator, AppLoggedInWithNavigator } from "router";
+import { store } from "store";
+import reactotron from "store/reactotron-config";
+import { apiQwerty, isTokenExist } from "utils";
+import { AppLoadingView } from "widgets/ModuleAppLoading";
+import { MyStatusBar } from "widgets/ModuleShared";
 
 interface IState {
 	tokenExist: Boolean;
@@ -33,9 +34,12 @@ export default class App extends Component<{}, IState> {
 		let hasToken;
 		try {
 			hasToken = await isTokenExist();
+			let locale = await AsyncStorage.getItem("locale");
+			reactotron.log({ locale });
+			store.dispatch({ type: "appState/setLanguage", payload: locale });
 		} catch (error) {}
 		apiQwerty.interceptors.request.use(val => {
-			val.headers = { ...val.headers, Authorization: !!hasToken ? hasToken : '' };
+			val.headers = { ...val.headers, Authorization: !!hasToken ? hasToken : "" };
 			return val;
 		});
 		this.setState({ tokenExist: !!hasToken, loading: false, token: hasToken });
@@ -48,10 +52,10 @@ export default class App extends Component<{}, IState> {
 		}
 		switch (tokenExist) {
 			case true: {
-				return <AppLoggedInWithNavigator />;
+				return <AppLoggedInWithNavigator key={store.getState().appState.appLang} />;
 			}
 			case false: {
-				return <AppAuthWithNavigator />;
+				return <AppAuthWithNavigator key={store.getState().appState.appLang} />;
 			}
 		}
 	};
